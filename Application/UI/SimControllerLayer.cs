ea662b09dff55f9a7766e5d7a94f54a4d2be6a19
jarrod <thejarroddoyle@gmail.com>
2022-10-12 09:46:02 +0100

    Moved random body generation to the World and added a UI button to call it

--- Application/UI/SimControllerLayer.cs
+++ Application/UI/SimControllerLayer.cs
@@ -4,6 +4,8 @@ namespace Application.UI;
 
 public class SimControllerLayer : Panel
 {
+    private int _numBodies = 1000;
+    
     public override void Attach()
     {
         Open = true;
@@ -22,6 +24,15 @@ public class SimControllerLayer : Panel
         ImGui.Begin("Sim Controller", ref isOpen);
         if (ImGui.Button("Reset")) World.Reset();
         ImGui.Checkbox("Playing", ref Program.Playing);
+        if (ImGui.Button("Generate Random Bodies"))
+        {
+            World.GenerateBodies(_numBodies);
+            World.Reset();
+        }
+        ImGui.SameLine();
+        if (ImGui.InputInt("##num_bodies", ref _numBodies))
+            _numBodies = Math.Clamp(_numBodies, 1, 1000);
+        ImGui.Value("Existing bodies", World.InitialBodies.Count);
         ImGui.End();
 
         if (!isOpen) Detach();
--- Application/World.cs
+++ Application/World.cs
@@ -1,3 +1,6 @@
+using System.Numerics;
+using Raylib_cs;
+
 namespace Application;
 
 public static class World
@@ -5,25 +8,41 @@ public static class World
     public static List<Body> InitialBodies { get; set; } = new();
     public static List<Body> Bodies { get; } = new();
     public static int SelectedIndex { get; set; } = -1;
-    private static List<Task> _tasks = new();
+    private static readonly List<Task> Tasks = new();
+    private static readonly Random Rnd = new();
 
     public static void Reset()
     {
         Bodies.Clear();
         foreach (var body in InitialBodies)
             Bodies.Add(new Body(body.Position, body.Velocity, body.Mass, body.Radius, body.Color));
-        _tasks.Capacity = InitialBodies.Count;
+        Tasks.Capacity = InitialBodies.Count;
     }
 
     public static void Update(float timeStep)
     {
-        _tasks.Clear();
+        Tasks.Clear();
         var bodiesArr = Bodies.ToArray();
         foreach (var body in Bodies)
-            _tasks.Add(Task.Run(() => body.UpdateVelocity(bodiesArr, timeStep)));
-        Task.WaitAll(_tasks.ToArray());
+            Tasks.Add(Task.Run(() => body.UpdateVelocity(bodiesArr, timeStep)));
+        Task.WaitAll(Tasks.ToArray());
 
         foreach (var body in Bodies)
             body.UpdatePosition(timeStep);
+    }
+
+    public static void GenerateBodies(int bodyCount)
+    {
+        for (var i = 0; i < bodyCount; i++)
+        {
+            var direction = new Vector3(Rnd.NextSingle(), Rnd.NextSingle(), Rnd.NextSingle());
+            var scaledNormalDirection = Vector3.Normalize(direction * 2 - Vector3.One);
+            var position = scaledNormalDirection * Rnd.NextSingle();
+            var velocity = new Vector3(Rnd.NextSingle(), Rnd.NextSingle(), Rnd.NextSingle());
+            var mass = Rnd.Next(1, 100);
+            var radius = Math.Clamp(Rnd.NextSingle() * 2, 0.2f, 2f);
+            var color = new Color(Rnd.Next(0, 256), Rnd.Next(0, 256), Rnd.Next(0, 256), 255);
+            InitialBodies.Add(new Body(position, velocity, mass, radius, color));
+        }
     }
 }
